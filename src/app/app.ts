4cd7dc33fb74fd7e1825fe454fdc2ab970fbea6f
Casaper <me@casaper.dev>
2025-09-15 22:00:44 +0200

    refactor: remove ngx-markdown

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
-  imports: [RouterModule, HeaderComponent, MarkdownComponent],
+  imports: [RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
