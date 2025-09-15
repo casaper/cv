5f4b94381eb2b4e9b0e2a6f19b32d355791ecaf1
Casaper <me@casaper.dev>
2025-09-15 22:29:01 +0200

    feat(app): router loaded md-converted lib components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'en',
+  },
+  {
+    path: 'en',
+    loadComponent: () =>
+      import('@cv/md-converted').then(m => m.MdConvertedEnComponent),
+  },
+  {
+    path: 'de',
+    loadComponent: () =>
+      import('@cv/md-converted').then(m => m.MdConvertedDeComponent),
+  },
+];
