85545d54ae923a5eb4919f4ceaffef7a78acccdc
Casaper <me@casaper.dev>
2025-09-16 16:55:02 +0200

    style: format project with new linter settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,12 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-
 import {
   provideRouter,
   withDebugTracing,
   withHashLocation,
 } from '@angular/router';
+
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { App } from './app';
 
 describe('App', () => {
